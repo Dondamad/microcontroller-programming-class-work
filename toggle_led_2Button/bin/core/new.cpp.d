b9f8289ecdf57bf350013fc1663c30985858bdb6
Romadon Damad <romadon4514@gmail.com>
2021-09-28 14:02:47 +0700

    Edit file

--- toggle_led_2Button/bin/core/new.cpp.d
+++
@@ -1,2 +0,0 @@
-d:\OneDrive\Dondamad\College_TSU\1_2564\MicroController_Prog\toggle_led\bin\core\new.cpp.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\new.cpp
--- toggle_led_2Button/bin/core/wiring.c.d
+++
@@ -1,6 +0,0 @@
-d:\OneDrive\Dondamad\College_TSU\1_2564\MicroController_Prog\toggle_led\bin\core\wiring.c.o: \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring.c \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\wiring_private.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\Arduino.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino\binary.h \
- C:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\standard/pins_arduino.h
